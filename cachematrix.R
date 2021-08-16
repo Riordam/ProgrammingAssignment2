a3b9c79d8c559d8e9dd43f795c62f5df68d8c297
Riordam <89016792+Riordam@users.noreply.github.com>
2021-08-16 12:05:32 +0100

    Programming assignment 2
    Programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,34 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## The solve function is used to calculate the inverse of the square matrix x
+## The square matrix x is assumed to be invertible
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) s <<- solve
+  getinv <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
-
+## Function will return 's' which is the inverse of the square matrix x
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+    s <- x$getsolve()
+    if(!is.null(s)) {
+      message("getting inverse of matrix")
+      return(s)
+    }
+    data <- x$get()
+    s <- solve(data, ...)
+    x$setsolve(s)
+    s
+  }
